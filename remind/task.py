b8b4ae891f58fc61e522167248d13c3743a3492a
Nitin Choudhary <nitin.iitkgp23@gmail.com>
2017-02-01 23:32:13 +0530

    Added task class

---
+++ remind/task.py
@@ -0,0 +1,25 @@
+import subprocess
+
+class Task():
+    ''' Task object stores details about the task to be scheduled or reminded of
+        
+        Parameters
+        ----------
+
+        name : Task keywords or short description
+        date : 
+        time :
+
+    '''
+
+    def __init__(self, name, date='345', time='345'):
+
+        self.name = name
+        self.date = date
+        self.time = time
+
+    def notify_Desktop(self):
+
+        subprocess.Popen(['notify-send',self.name])        
+        os.system('aplay -q ~/remind/res/notifications/Mallet.wav')
+
